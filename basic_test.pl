b3c308afc825b2fea28c6e130ad1c5f50d8a64ff
David Bonner <dbonner@gmail.com>
2013-05-08 15:22:17 -0400

    webhdfs doesn't support symlinks or ctime, don't test those

--- basic_test.pl
+++ basic_test.pl
@@ -3,5 +3,11 @@
 use Test::Virtual::Filesystem;
 
 my $tester = Test::Virtual::Filesystem->new({mountdir => $ARGV[0]});
+
+# hdfs doesn't really support symlinks correctly
 $tester->enable_test_symlink(0);
+
+# hdfs doesn't have a concept of ctime they expose
+$tester->enable_test_ctime(0);
+
 $tester->runtests;
